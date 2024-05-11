5866abaee28402693615b88d1ef6b5d6706ec0e6
TheRockex <115924688+TheRockex@users.noreply.github.com>
2024-05-11 20:36:03 +0200

    try 8

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParse = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParse.urlencoded({ extended: true}));
-app.use(bodyParse.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 const mysql = require('mysql');
 
