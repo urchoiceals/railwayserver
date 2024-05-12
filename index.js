41d31b8c89faedef2c0255aa27fc5d4f64819f64
TheRockex <115924688+TheRockex@users.noreply.github.com>
2024-05-12 18:15:47 +0200

    DE VUELTA

--- index.js
+++ index.js
@@ -139,7 +139,8 @@ app.get("/elements/:categoryId", (req, res) => {
 });
 
 //--------------------------------------CATEGORIES-------------------------------------------------------------------------------------------------------
-
+  
+  
 app.get("/categories", (req, res) => {
     connection.query('SELECT id_cat, name_cat, img_cat FROM categories', (error, results) => {
         if (error) {
@@ -162,79 +163,8 @@ app.get("/categories", (req, res) => {
 });
 
 
+
 app.post("/categories/create", (req, res) => {
-    const { name_cat, img_cat, elements } = req.body;
-
-    // Convertir la imagen de base64 a bytes
-    const imgBytes = Buffer.from(img_cat, 'binary');
-
-    // Comenzar una transacción
-    connection.beginTransaction(function(err) {
-        if (err) {
-            console.error('Error al iniciar la transacción:', err);
-            return res.status(500).json({ error: 'Error interno del servidor al iniciar la transacción'});
-        }
-
-        // Insertar la categoría
-        connection.query('INSERT INTO categories (name_cat, img_cat) VALUES (?, ?)', [name_cat, imgBytes], (error, categoryResult) => {
-            if (error) {
-                connection.rollback(function() {
-                    console.error('Error al insertar la nueva categoría:', error);
-                    return res.status(500).json({ error: 'Error interno del servidor al insertar categoría' });
-                });
-                return; // Detener la ejecución en caso de error
-            }
-
-            const id_cat = categoryResult.insertId; // Obtener el ID de la categoría recién insertada
-
-            let query = 'INSERT INTO elements (img_elem, name_elem) VALUES ?';     
-            let elementValues = elements.map(element => [Buffer.from(element.img_elem, 'binary'), element.name_elem]);
-
-            connection.query(query, [elementValues], (error, elementResult) => {
-                if (error) {
-                    connection.rollback(function() {
-                        console.error('Error al insertar los elementos:', error);
-                        return res.status(500).json({ error: 'Error interno del servidor al insertar elementos' });
-                    });
-                    return; // Detener la ejecución en caso de error
-                }
-
-                // Obtener los ID de los elementos recién insertados
-                const elementIds = elementResult.insertId; // Este enfoque no es correcto
-
-                // Construir los valores para la tabla intermedia
-                let elemCatValues = elements.map(element => [elementIds, id_cat, element.victories]);
-
-                // Insertar en la tabla intermedia id_elemcat
-                connection.query('INSERT INTO elemcat (id_elem, id_cat, victories) VALUES ?', [elemCatValues], (error, elemCatResult) => {
-                    if (error) {
-                        connection.rollback(function() {
-                            console.error('Error al insertar en la tabla intermedia:', error);
-                            return res.status(500).json({ error: 'Error interno del servidor al insertar en tabla intermedia' });
-                        });
-                        return; // Detener la ejecución en caso de error
-                    }
-
-                    // Commit de la transacción si todas las inserciones fueron exitosas
-                    connection.commit(function(err) {
-                        if (err) {
-                            connection.rollback(function() {
-                                console.error('Error al hacer commit de la transacción:', err);
-                                return res.status(500).json({ error: 'Error interno del servidor al hacer commit de la transacción' });
-                            });
-                            return; // Detener la ejecución en caso de error
-                        }
-
-                        console.log('Transacción completada con éxito.');
-                        res.status(200).json({ message: 'Transacción completada con éxito.' });
-                    });
-                });
-            });
-        });
-    });
-});
-
-/*app.post("/categories/create", (req, res) => {
     const { name_cat, img_cat, elements } = req.body;
 
     // Convertir la imagen Base64 a bytes
@@ -304,7 +234,7 @@ app.post("/categories/create", (req, res) => {
             });
         });
     });
-});*/
+});
 
 
 
