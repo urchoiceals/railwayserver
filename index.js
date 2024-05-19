5e3e2efde75f7f780ba65c4cb3fece985a202ab4
TheRockex <115924688+TheRockex@users.noreply.github.com>
2024-05-19 19:54:48 +0200

    TRY 9000

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParse = require("body-parser");
+const bcrypt = require('bcrypt');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -39,7 +40,6 @@ app.post("/user/register", (req, res) => {
             return res.status(400).json({ error: "El usuario ya existe" });
         }
 
-        // Convertir la imagen a bytes
         const imgBytes = Buffer.from(img, 'base64');
 
         connection.query('INSERT INTO users (email_user, nick_user, pass_user, img_user) VALUES (?, ?, ?, ?)', [email, nick, contra, imgBytes], (error, results) => {
@@ -54,7 +54,6 @@ app.post("/user/register", (req, res) => {
 
                 const insertedUser = results[0];
                 
-                // Tratar la imagen del usuario en base64
                 const imgBase64 = insertedUser.img_user.toString('base64');
 
                 const userWithBase64Image = {
@@ -78,8 +77,8 @@ app.post("/user/register", (req, res) => {
 
 app.post("/user/login", (req, res) => {
     const { email, contra } = req.body;
-
-    connection.query('SELECT * FROM users WHERE email_user = ? AND pass_user = ?', [email, contra], (error, results) => {
+  
+    connection.query('SELECT * FROM users WHERE email_user = ?', [email], (error, results) => {
       if (error) {
         console.error('Error al realizar la consulta:', error);
         return res.status(500).json({ error: 'Error interno del servidor' });
@@ -88,26 +87,35 @@ app.post("/user/login", (req, res) => {
         return res.status(401).json({ error: "Credenciales incorrectas" });
       }
       const user = results[0];
-      
-      // Tratar la imagen del usuario en base64
-      const imgBytes = user.img_user;
-      let imgBase64 = null; // Inicializa imgBase64 como null
+  
+      bcrypt.compare(contra, user.pass_user, (err, result) => {
+        if (err) {
+          console.error('Error al comparar contraseñas hasheadas:', err);
+          return res.status(500).json({ error: 'Error interno del servidor' });
+        }
+        if (!result) {
+          return res.status(401).json({ error: "Credenciales incorrectas" });
+        }
+
+        const imgBytes = user.img_user;
+      let imgBase64 = null;
       if (imgBytes !== null) {
           imgBase64 = Buffer.from(imgBytes).toString('base64');
       }
-
-      const userWithBase64Image = {
-        id_user: user.id_user,
-        email_user: user.email_user,
-        nick_user: user.nick_user,
-        pass_user: user.pass_user,
-        img_user: imgBase64,
-        GamesPlayed: user.GamesPlayed
-      };
-      
-      res.status(200).json(userWithBase64Image);
+        
+        const userWithBase64Image = {
+            id_user: user.id_user,
+            email_user: user.email_user,
+            nick_user: user.nick_user,
+            pass_user: user.pass_user,
+            img_user: imgBase64,
+            GamesPlayed: user.GamesPlayed
+          };
+          
+          res.status(200).json(userWithBase64Image);
+      });
     });
-});
+  });
 
 
 app.get("/users/all/:id_user", (req, res) => {
