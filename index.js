866e8adf10c0731f99e1e368704989870bf33124
LucaCampusfp <urchoice.als@gmail.com>
2025-03-11 10:21:19 +0100

    prueba-21

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const connection = mysql.createPool({
   });
   
 
-connection.connect((err) => {
+connection.getConnection((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err.stack);
     return;
